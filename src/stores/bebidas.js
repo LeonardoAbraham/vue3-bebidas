decf1ab00f59a56c5da4ab5afc8b5d1a93a86c6c
Leonardo Alonzo <leonardo.a@cliker.mx>
2023-11-28 13:38:11 -0600

    Consultando la API con los valores ingresados en el Formulario

--- src/stores/bebidas.js
+++ src/stores/bebidas.js
@@ -9,6 +9,7 @@ export const useBebidasStore = defineStore('bebidas', ()=> {
         nombre: '',
         categoria: ''
     })
+    const recetas = ref([])
 
     onMounted(async function() {
         const {data: {drinks}} = await APIService.obtenerCategorias()
@@ -16,8 +17,10 @@ export const useBebidasStore = defineStore('bebidas', ()=> {
         categorias.value = drinks
     })
 
-    function obtenerRecetas() {
-        console.log('Consultando API...')
+    async function obtenerRecetas() {
+        const {data: {drinks}} = await APIService.buscarRecetas(busqueda)
+        recetas.value = drinks
+        
     }
 
     return {
