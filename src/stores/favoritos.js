0b53d64c7e53350b44da05ad2b5e9743c956e04c
Leonardo Alonzo <leonardo.a@cliker.mx>
2023-11-29 15:36:04 -0600

    Creando el Store de Favoritos

---
+++ src/stores/favoritos.js
@@ -0,0 +1,14 @@
+import { defineStore } from "pinia";
+
+export const useFavoritosStore = defineStore('favoritos', ()=> {
+    
+
+    const handleClickFavorito = () => {
+        console.log('agregando')
+    }
+
+
+    return {
+        handleClickFavorito
+    }
+})
