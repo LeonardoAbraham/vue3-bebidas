b5f16b4905ccbbe1277a84903060ba688fdb5e68
Leonardo Alonzo <leonardo.a@cliker.mx>
2023-12-01 11:32:52 -0600

    Mostrando notificación cuando un elemento se elimina

--- src/stores/favoritos.js
+++ src/stores/favoritos.js
@@ -33,6 +33,10 @@ export const useFavoritosStore = defineStore('favoritos', ()=> {
 
     function eliminarFavorito() {
         favoritos.value = favoritos.value.filter(favorito => favorito.idDrink !== bebidas.receta.idDrink)
+
+        notificaciones.mostrar = true
+        notificaciones.texto = 'Eliminado de favoritos'
+
     }
 
     function agregarFavorito() {
@@ -41,9 +45,6 @@ export const useFavoritosStore = defineStore('favoritos', ()=> {
         notificaciones.mostrar = true
         notificaciones.texto = 'Se agregó a favoritos'
 
-        setTimeout(()=> {
-            notificaciones.$reset()
-        }, 3000)
     }
 
     function handleClickFavorito() {
